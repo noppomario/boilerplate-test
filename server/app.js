5820e08bf7a6d6062c07f42b27e8aad05220dd99
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-02-27 00:48:37 +0900

    Fix post error at api server

--- server/app.js
+++ server/app.js
@@ -1,51 +1,36 @@
 "use strict";
-//var koa = require('koa');
-//var router = require('koa-router');
-//var serve = require('koa-static');
-//var mount = require('koa-mount');
-//var app = koa();
 
 var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
 
-//app.use(router(app));
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/animals', function (req, res){
-  console.log('HOAAAAAAAAAA');
-  res.set({'Content-Type': 'application/json'});
-  res.send( JSON.stringify({
-    "total": 3,
-    "list": [
-      {animalId: 1, name:'Gorilla'},
-      {animalId: 5, name:'Dog'},
-      {animalId: 7, name:'Cat'},
-    ]}) );  
-});
+var router = express.Router();
 
-app.put('/animals/:id', function (req, res){
-  console.log('PUT');
-  res.set({'Content-Type': 'application/json'});
-  console.log(req.body);
-  res.send(req.body);
-});
+router.route('/users')
+  .get(function (req, res){
+    console.log('GET');
+    res.set({'Content-Type': 'application/json'});
+    res.json({
+      "total": 3,
+      "list": [
+        {userId: 1, name:'Mike'},
+        {userId: 5, name:'Mary'},
+        {userId: 7, name:'Bob'},
+      ]} );  
+  })
+  .post(function (req, res){
+    console.log('POST');
+    console.log(req.body);
+    res.set({'Content-Type': 'application/json'});
+    res.send(req.body.name);
+  });
 
-app.post('/animals', function (req, res){
-  console.log('POST');
-  res.set({'Content-Type': 'application/json'});
-  console.log(req.body);
-  res.send(req.body);
-});
+app.use('/', router);
 
-//app.use(router.routes())
-//   .use(router.allowedMethods());
-
-//console.log(__dirname);
-//app.use(serve(__dirname + '/../app'));
 app.use(express.static(__dirname + '/../app'));
 
 app.listen(4649);
-
 
