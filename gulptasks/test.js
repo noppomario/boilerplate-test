ac86b89e9d4785cf20c24b3699b730852e9cfd2b
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-03-23 10:49:51 +0900

    refactoring gulp tests

--- gulptasks/test.js
+++ gulptasks/test.js
@@ -5,30 +5,16 @@ var personal = require('../personalSettings.json');
 module.exports = function(gulp, path){
   'use strict';
 
-  // Gulp Utils
-  const eventStream = require('event-stream');
-  const runSequence = require('run-sequence');
-  const source      = require('vinyl-source-stream');
-
-  // Module
-  const browserify = require('browserify');
-  const nodeUnderscorify = require('node-underscorify');
-
-  // Test
-  const karma = require('karma').server;
-  const mocha = require("gulp-mocha");
-  const espower = require("gulp-espower");
-  const ts =    require("gulp-typescript");
-  const sourcemaps  = require('gulp-sourcemaps');
-  const espowerify  = require('espowerify');
-  //const plumber     = require('gulp-plumber');
-
   function handleError(err){
     console.log(err.toString());
     this.emit('end');
   }
 
   function _compileTest (paths) {
+    const eventStream = require('event-stream');
+    const ts =    require("gulp-typescript");
+    const sourcemaps  = require('gulp-sourcemaps');
+
     const tsResult = gulp.src(paths)
 	  .pipe(sourcemaps.init())
 	  .pipe(ts({
@@ -47,19 +33,11 @@ module.exports = function(gulp, path){
   }
 
   gulp.task("power-assert", function() {
+    const espower = require("gulp-espower");
+
     return gulp.src('app/compiled-tests/**/*.js')
 	       .pipe(espower())
 	       .pipe(gulp.dest('app/powered-tests'));
-  });
-
-  gulp.task("power-assert-remote", function() {
-    return browserify('./app/compiled-tests/tests/index.js', {
-	debug:true,
-      })
-     .transform([nodeUnderscorify, espowerify])
-      .bundle()
-      .pipe(source('index.js'))
-      .pipe(gulp.dest('app/powered-tests'));
   });
 
   gulp.task('compile-test', function(){
@@ -87,16 +65,22 @@ module.exports = function(gulp, path){
   });
 
   gulp.task("_test:local", function(){
+    const mocha = require("gulp-mocha");
+
     return gulp.src('app/powered-tests/tests/**/*.js', {read: false })
 	.pipe(mocha({ reporter: 'spec' }))
 	.on('error', handleError);
   });
 
   gulp.task("test:local", function(){
+    const runSequence = require('run-sequence');
+
     runSequence('clean:test', 'compile-test-local', 'power-assert', '_test:local');
   });
 
   gulp.task('_test:remote', function(done){
+    const karma = require('karma').server;
+
     return karma.start({
       configFile: __dirname + '/../karma.conf.js',
       singleRun: false
@@ -104,10 +88,14 @@ module.exports = function(gulp, path){
   });
 
   gulp.task('test:remote', function(done){
-    runSequence('clean:test', 'compile-test', /*'power-assert-remote',*/ '_test:remote');
+    const runSequence = require('run-sequence');
+
+    runSequence('clean:test', 'compile-test', '_test:remote');
   });
 
   gulp.task('_test:phantom', function(done){
+    const karma = require('karma').server;
+
     return karma.start({
       configFile: __dirname + '/../karma.conf.js',
       browsers: ['PhantomJS'],
@@ -116,7 +104,9 @@ module.exports = function(gulp, path){
   });
 
   gulp.task('test:phantom', function(done){
-    runSequence('clean:test', 'compile-test', /*'power-assert-remote',*/ '_test:phantom');
+    const runSequence = require('run-sequence');
+
+    runSequence('clean:test', 'compile-test', '_test:phantom');
   });
 
 };
