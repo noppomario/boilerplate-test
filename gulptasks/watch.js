b93b869f46ccfa6c78b880d50db4f9c743bd3546
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-03-30 17:05:29 +0900

    remove browser-sync

--- gulptasks/watch.js
+++ gulptasks/watch.js
@@ -3,13 +3,13 @@ module.exports = function(gulp, path, argv){
   'use strict';
 
   const watch = require('gulp-watch');
-  const browserSync = require('browser-sync');
+//  const browserSync = require('browser-sync');
 
   if( argv.indexOf('ts') !== -1 ){
     console.log('watch: typescript files');
     watch(path.tsFiles, function(){
       gulp.start(['compile-all']);
-      browserSync.reload();
+//      browserSync.reload();
     });
   }
 };
