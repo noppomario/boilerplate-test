07f2922c52c11551cb9c59ec695331cc8275c0bb
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-04-02 17:57:14 +0900

    tochu

--- gulptasks/handleErrors.js
+++ gulptasks/handleErrors.js
@@ -5,7 +5,15 @@ module.exports = function(){
   const notify  = require('gulp-notify');
 
   var args = Array.prototype.slice.call(arguments);
+  /*
+  if (args[0].message != undefined){
+    var q = args[0].message.replace(/\u001b\[[0-9]+m/g,'');
+    args[0].message = q;
+  }
+  console.log(args[0]);
 
+  console.log(Object.keys(args[0]));
+  */
   notify.onError({
     title: "Compile Error",
     message: "<%= error %>",
